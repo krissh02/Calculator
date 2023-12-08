e30779880b30e2a79b5aecbd0a5a22373a4e42cd
KrishnaSabat <202003053.krishnasrs@student.xavier.ac.in>
2023-12-08 19:22:14 +0530

    changes done

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 let inp = document.querySelector("#input");
 let btn = document.querySelectorAll("button");
 let calc = document.querySelector(".calculator");
-// let flag = true;
+let operator = document.querySelectorAll(".op");
+// console.log(operator);
+
 let string = "";
 let arr = Array.from(btn);
+// console.log(arr);
+
 arr.forEach((button) =>{
     button.addEventListener("click",(e)=>{
         if(e.target.innerHTML=="="){
+            if(string.charAt(string.length-1) == "%" || string.charAt(string.length-1) == "/" || string.charAt(string.length-1) == "*" || string.charAt(string.length-1) == "+" || string.charAt(string.length-1) == "-" ){
+                alert("Invalid number");
+            }
             string = eval(string);
             input.value = string;
         }
@@ -18,16 +25,6 @@ arr.forEach((button) =>{
             string = string.substring(0,string.length-1);
             input.value = string;
         }
-        // else if(e.target.innerHTML == "ON"){
-        //     if(flag){
-        //         calc.style.background = "white";
-        //         flag = false;
-        //     }
-        //     else{
-        //         calc.style.background = "transparent";
-        //         flag = true;
-        //     }
-        // }
         else{
         string += e.target.innerHTML;
         input.value = string;
